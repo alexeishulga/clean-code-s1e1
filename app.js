5a314734c7115d5bbf83acc877e238946c05033e
alexeishulga <alexeishulg@gmail.com>
2021-04-03 21:25:38 +0300

    fix:final check v2 according to rule 2.2 in html-and-css.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("form-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("form-task_incomplete");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("form-task_completed");//completed-tasks
@@ -85,7 +85,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".menu-task__buttonEdit");
-    var containsClass=listItem.classList.contains("menu__item");
+    var containsClass=listItem.classList.contains("editMode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("menu__item");
+    listItem.classList.toggle("editMode");
 };
 
 
